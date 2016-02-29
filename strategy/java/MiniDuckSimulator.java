ede6b55c9444aa99661155e09500b8e59d87d988
DanielFroz <kharchilindanila@gmail.com>
2016-02-29 18:34:47 +0400

    initial MiniDuckSimulator commit

---
+++ strategy/java/MiniDuckSimulator.java
@@ -0,0 +1,8 @@
+import ducks.*;
+
+public class MiniDuckSimulator {
+	public static void main(String args[]) {
+		Duck d = new Duck();
+		d.swim();
+	}
+}
---
+++ strategy/java/ducks/Duck.java
@@ -0,0 +1,7 @@
+package ducks;
+
+public class Duck {
+	public void swim() {
+		System.out.println("I am swim like decoy!");
+	}
+}
